d8f7b4ecea1ba4cfc4d66be3ea5921def4e273fd
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-03-07 09:44:46 +0000

    prettier

--- src/Components/SongList.jsx
+++ src/Components/SongList.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { selectSong } from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
-    renderList() {
-        return this.props.songs.map(song => {
-            return (
-                <div className="item" key={song.title}>
-                    <div className="right floated content">
-                        <button 
-                            onClick={() => {this.props.selectSong(song)}}
-                            className="ui button primary"
-                        >Select</button>
-                    </div>
-                    <div className="content">{song.title}</div>
-                </div>
-            )
-        });
-    }
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              onClick={() => {
+                this.props.selectSong(song);
+              }}
+              className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
 
-    render() {
-        return (
-            <div className="ui divided list">
-                {this.renderList()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
 }
 
-const mapStateToProps = (state) => {
-    return { songs: state.songs };
-}
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
 // v^ we are mapping redux store (~state) to component props
 export default connect(mapStateToProps, { selectSong })(SongList);
