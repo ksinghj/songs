376b40af42f333d8dfdb13fc94014cd2c4e1c84c
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2020-03-09 19:18:18 +0000

    Update SongDetail.jsx

--- src/Components/SongDetail.jsx
+++ src/Components/SongDetail.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 const SongDetail = ({ song }) => {
-    if (!song) {
-        return <div>Select a song</div>;
-    } 
-    
-    return ( 
-        <div>
-            <h3>Details for:</h3>
-            <p>Title: {song.title}</p>
-            <br />
-            <p>Duration: {song.duration}</p>
-        </div>
-    )
-}
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>Title: {song.title}</p>
+      <br />
+      <p>Duration: {song.duration}</p>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-    return { song: state.selectedSong };
-}
+  return { song: state.selectedSong };
+};
 
 export default connect(mapStateToProps)(SongDetail);
